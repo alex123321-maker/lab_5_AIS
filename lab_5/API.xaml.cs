848cf59d4072e6c5eb747e63c0815ae19490651f
Александр Мандриков <alexander12110@gmail.com>
2023-12-27 19:45:38 +0700

    Добавьте файлы проекта.

---
+++ lab_5/API.xaml.cs
@@ -0,0 +1,175 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace lab_5
+{
+    /// <summary>
+    /// Логика взаимодействия для API.xaml
+    /// </summary>
+    public partial class API : Window
+    {
+        public string accesstoken { get; set; }
+
+        public API()
+        {
+            InitializeComponent();
+        }
+        private async Task<string> SendRequestAsync(string baseUrl, string method, Dictionary<string, string> parameters = null)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Создаем HttpRequestMessage
+                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, $"{baseUrl}{method}");
+
+
+
+                requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accesstoken);
+
+
+
+                // Добавляем параметры к URL
+                if (parameters != null)
+                {
+                    string queryString = string.Join("&", parameters.Select(p => $"{p.Key}={p.Value}"));
+                    requestMessage.RequestUri = new Uri(requestMessage.RequestUri + "?" + queryString);
+                }
+
+                // Отправляем запрос и получаем ответ
+                HttpResponseMessage response = await client.SendAsync(requestMessage);
+
+                // Проверяем успешность запроса
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    // Обработка ошибок
+                    return $"Error: {response.StatusCode}";
+                }
+            }
+        }
+
+        private void DisplayJsonInTextBlock(string jsonString)
+        {
+            try
+            {
+                // Парсим JSON-строку в объект JToken
+                JToken json = JToken.Parse(jsonString);
+
+                // Сериализуем объект JToken в красиво отформатированную JSON-строку
+                string formattedJson = json.ToString(Formatting.Indented);
+
+                // Отображаем JSON в TextBlock
+                textBox.Text = formattedJson;
+            }
+            catch (JsonException ex)
+            {
+                // Обработка ошибок парсинга JSON
+                textBox.Text = $"{jsonString}";
+            }
+        }
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string apiUrl = "https://osu.ppy.sh/api/v2";
+                string method = "/users/34427975/osu";
+
+                Dictionary<string, string> parameters = new Dictionary<string, string>
+                {
+
+                };
+
+                // Заголовки запроса (если необходимо)
+
+
+                string result = await SendRequestAsync(apiUrl, method, parameters);
+                DisplayJsonInTextBlock(result);
+
+            }
+            catch (Exception ex)
+            {
+                // Обработка исключений
+                textBox.Text = $"Error: {ex.Message}";
+            }
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string apiUrl = "https://osu.ppy.sh/api/v2";
+                string method = "/users/34427975/beatmapsets/most_played";
+
+                Dictionary<string, string> parameters = new Dictionary<string, string>
+                {
+                    {"limit","10"}
+                };
+
+                // Заголовки запроса (если необходимо)
+
+
+                string result = await SendRequestAsync(apiUrl, method, parameters);
+                DisplayJsonInTextBlock(result);
+
+            }
+            catch (Exception ex)
+            {
+                // Обработка исключений
+                textBox.Text = $"Error: {ex.Message}";
+            }
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string apiUrl = "https://osu.ppy.sh/api/v2";
+                string method;
+                Dictionary<string, string> parameters = new Dictionary<string, string>
+                {
+                    
+                };
+                if (!string.IsNullOrEmpty(inputTextBox.Text))
+                {
+                    method = $"/beatmaps/lookup";
+                    parameters.Add("id", $"{inputTextBox.Text}");
+
+                }
+                else
+                {
+                    inputTextBox.Text = "Введите id карты";
+                    return;
+                }
+
+
+                // Заголовки запроса (если необходимо)
+
+
+                string result = await SendRequestAsync(apiUrl, method, parameters);
+                DisplayJsonInTextBlock(result);
+
+            }
+            catch (Exception ex)
+            {
+                // Обработка исключений
+                textBox.Text = $"Error: {ex.Message}";
+            }
+        }
+    }
+}
+
---
+++ lab_5/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web; // Для использования HttpUtility
+using System.Windows;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using CefSharp.Web;
+using System.Net.Http.Headers;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace lab_5
+{
+    public partial class MainWindow : Window
+    {
+        private bool isWindowOpened = false;
+        readonly string appId = "28519";
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Browser.LoadingStateChanged += Browser_LoadingStateChanged;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var uriStr = @"https://osu.ppy.sh/oauth/authorize?client_id=" + appId +
+                @"&response_type=code&scope=public identify&redirect_uri=http://localhost:4000";
+            Browser.Load(uriStr);
+        }
+
+        private async void Browser_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            if (e.IsLoading == false )
+            {
+                string url = e.Browser.MainFrame.Url;
+                Uri uri = new Uri(url);
+                NameValueCollection queryParams = HttpUtility.ParseQueryString(uri.Query);
+
+                string code = queryParams["code"];
+                
+
+                Dispatcher.Invoke(() => Output.Text = url);
+
+                if (!string.IsNullOrEmpty(code) && !isWindowOpened)
+                {
+                    Dispatcher.Invoke(() => this.Hide());
+                    
+                    string token = await GetAccessToken(code);
+                    if (token != null)
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            API apiWindow = new API();
+                            apiWindow.accesstoken = token;
+                            apiWindow.Show();
+                        });
+                    }
+
+
+
+                }
+            }
+        }
+        private async Task<string> GetAccessToken(string code)
+        {
+
+
+            using (var httpClient = new HttpClient())
+            {
+                var requestContent = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("client_id", appId),
+                    new KeyValuePair<string, string>("client_secret", "eJ13YQPX2OWC8rwmV5EIbxsRdmurOYOoBlTcZG1J"),
+                    new KeyValuePair<string, string>("code", code),
+                    new KeyValuePair<string, string>("grant_type", "authorization_code"),
+                    new KeyValuePair<string, string>("redirect_uri", "http://localhost:4000"),
+                });
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                using (var response = await httpClient.PostAsync("https://osu.ppy.sh/oauth/token", requestContent))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+
+                        var content = await response.Content.ReadAsStringAsync();
+
+                        Dispatcher.Invoke(() => Output.Text = content);
+                        var tokenObject = JsonConvert.DeserializeObject<JObject>(content);
+
+                        // Получение значения access_token
+                        string accessToken = tokenObject["access_token"].ToString();
+                        return accessToken;
+                    }
+                    return null;
+                }
+            }
+        }
+        private void Browser_AddressChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+           
+        }
+    }
+}
